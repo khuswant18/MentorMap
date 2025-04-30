70b15eceb0a702c65435714b83bb71868273affd
nst-student <khuswantrajpurohit18@gmail.com>
2025-04-30 19:51:05 +0530

    updated FloatingCard

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './Profile.css';
-import BookSession from "../Booksession/BookSession";
+import BookSession from "../BookSession/BookSession";
 
 const studentData = {
   studentDetails: [
